89600d3e924cf549633921a2eeba1773cd8c245d
Rahim Pormah <sas4n.porm4h@gmail.com>
2022-10-19 16:48:00 +0200

    The logic of getting all issus from gitlab and sending it to client through socket added

--- app.js
+++ app.js
@@ -10,16 +10,70 @@ const io = require('socket.io')(server)
 app.use(express.urlencoded({extended : false}))
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/', (req, res) => {
+io.on('connection', async(socket) => {
+    console.log('user connected')
+    const config = {
+        headers:{
+            Authorization:` Bearer ${process.env.GITLAB_ACCESS_TOKEN}`
+        }
+    }
+    const {data} = await axios.get(process.env.GITLAB_URL, config)
+    const allIssuesData = data.map(issue => ({
+        id: issue.id,
+        project_id : issue.project_id,
+        title : issue.title,
+        description : issue.description,
+        state: issue.state,
+        closed_by : issue.closed_By ? issue.closed_By.name: null,
+        labels: issue.labels,
+        assignees: issue.assignees.map((assignee) => ({assignee_userName: assignee.username})),
+        author_name: issue.author.name,
+        type: issue.type,
+        assignee : issue.assignee ? issue.assignee.name : null,
+        upvotes: issue.upvotes,
+        downvotes: issue.downvotes,
+        due_date: issue.due_date,
+        web_url: issue.web_url,
+        project_url: issue.project,
+
+    }))
+    socket.emit('allIssues', allIssuesData)
+
+} 
+    
+    )
+
+/*app.get('/', async(req, res) => {
     console.log('get is called')
     //res.set('PRIVATE-TOKEN', process.env.GITLAB_ACCESS_TOKEN)
     const config = {
         headers:{Authorization:` Bearer ${process.env.GITLAB_ACCESS_TOKEN}`  }
     }
-    axios.get(process.env.GITLAB_URL, config).then((response) => {res.send(response.data)}).catch((error) => {console.log(error)})
+     const {data} = await axios.get(process.env.GITLAB_URL, config)
+    const allIssuesData = data.map(issue => ({
+        id: issue.id,
+        project_id : issue.project_id,
+        title : issue.title,
+        description : issue.description,
+        state: issue.state,
+        closed_by : issue.closed_By ? issue.closed_By.name: null,
+        labels: issue.labels,
+        assignees: issue.assignees.map((assignee) => ({assignee_userName: assignee.username})),
+        author_name: issue.author.name,
+        type: issue.type,
+        assignee : issue.assignee ? issue.assignee.name : null,
+        upvotes: issue.upvotes,
+        downvotes: issue.downvotes,
+        due_date: issue.due_date,
+        web_url: issue.web_url,
+        project_url: issue.project,
+
+    }))
+    res.send(allIssuesData)
+   // axios.get(process.env.GITLAB_URL, config).then((response) => {res.send(response.data)}).catch((error) => {console.log(error)})
    // res.send() kj
 
-})
+})*/
 
 app.post('/gitlab', (req, res) => {
     console.log(req.headers)
--- public/index.js
+++ public/index.js
@@ -4,9 +4,17 @@ const flexContainer = document.querySelectorAll('.flex-container')[0]
 
 
 socket.on('allIssues', (issueData) => {
+    //issueData = JSON.parse(issueData)
+    console.log('hi')
+    console.log(issueData)
     issueData.forEach((issue) => {
         const issueDiv = document.createElement('div')
         issueDiv.classList.add('flex-item')
+        const header = document.createElement('h2')
+        header.textContent = issue.title
+        issueDiv.appendChild(header)
         flexContainer.appendChild(issueDiv)
+        
+        
     })
 })
